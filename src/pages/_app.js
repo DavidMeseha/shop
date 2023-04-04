bf9e3463ead6a8fcaffe4911512e54ac194b861f
DavidMeseha <111684897+DavidMeseha@users.noreply.github.com>
2023-04-04 12:21:16 +0200

    Shop

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,15 @@
-import '@/styles/globals.css'
+import { FilterStateProvider } from '@/context/filterProvider'
+import { UserProvider } from '@/context/userProvider'
+import NavBar from '@/layouts/NavBar'
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <FilterStateProvider>
+        <nav><NavBar /></nav>
+        <Component {...pageProps} />
+      </FilterStateProvider>
+    </UserProvider>
+  )
 }
